066c9f7359fd1d184dccb9d51ca9c23cd5881750
Tom Carlson <tcarlson@rbmtechnologies.com>
2017-05-14 22:02:06 -0400

    chapter 5, part 2, scss and fix links

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -21,6 +21,12 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :about
     assert_response :success
     assert_select "title", "About | #{@base_title}"
-  end  
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 
 end
