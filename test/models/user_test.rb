fc62e9bee666e72d23c73ba571be92f199dafc43
Tom Carlson <tcarlson@rbmtechnologies.com>
2017-05-18 21:04:28 -0400

    chapter 6: authentication

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,26 +2,31 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Riley", email: "riley@woof.com")
+    @user = User.new(name: "Riley", email: "riley@woof.com", password: 'foobar')
   end
+
+  # **************************************
 
   test "should be valid" do
     assert @user.valid?
   end
+
 
   test "name should be present" do
     @user.name = ""
     assert_not @user.valid?
   end
 
-  test "email should be present" do
-    @user.email = ""
-    assert_not @user.valid?
-  end
-
   test "name should not be too long" do
     @user.name = 'a' * 51
     assert_not @user.valid?    
+  end
+
+  # **************************************
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
   end
 
   test "email should not be too long" do
@@ -65,7 +70,18 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert @user.email == "misty@meow.com"
   end
-  
+
+  # **************************************
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password have a minimum length" do
+    @user.password = @user.password_confirmation = " " * 5
+    assert_not @user.valid?
+  end  
 end
 
 
