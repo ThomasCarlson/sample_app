1750f25f3f8baa1f950edac0ffde0343486be843
Tom Carlson <tcarlson@rbmtechnologies.com>
2017-05-30 20:45:49 -0400

    ssl, puma server in production, new files not added previously

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    before_count = User.count
+    get signup_path
+    post users_path, user: { name:  "",
+                             email: "user@invalid",
+                             password:              "foo",
+                             password_confirmation: "bar" }
+    after_count = User.count    
+    assert_equal before_count, after_count
+  end
+
+  test "valid signup information" do
+    before_count = User.count
+    get signup_path
+    post_via_redirect users_path, user: { name:  "Riley",
+                             email: "riley@woof.com",
+                             password:              "foobar",
+                             password_confirmation: "foobar" }
+    after_count = User.count
+    assert_equal after_count, before_count + 1
+    assert_template 'users/show'
+  end
+
+end
+
+
