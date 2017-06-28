20b432811c1b49ef4b392b0c625bca8d43e47cd2
Tom Carlson <tcarlson@rbmtechnologies.com>
2017-06-28 07:14:50 -0400

    micropost initial implementation

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,4 +40,10 @@ User.create!(name:  "Maggie Carlson",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+
+  users = User.order(:created_at).take(6)
+  50.times do
+    content = Faker::Lorem.sentence(5)
+    users.each { |user| user.microposts.create!(content: content) }
+  end
 end
